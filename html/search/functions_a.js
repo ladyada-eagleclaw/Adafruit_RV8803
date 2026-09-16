d00212430a4ab4ef3738a77fece49e755c73ceb0
Doxygen CI <ci-arduino@invalid>
2026-09-16 20:36:01 +0000

    Deploy docs to GitHub Pages from commit cc1bb15b70
    Commit: cc1bb15b7037badc231332b7a61666333ca2c05b
    GitHub Actions run: 35146953744

--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -4,7 +4,6 @@ var searchData=
   ['readeventcontrol',['readEventControl',['../class_adafruit___r_v8803.html#acdaabb62d8b0d4bb49d85abf15497ed1',1,'Adafruit_RV8803']]],
   ['readextensionregister',['readExtensionRegister',['../class_adafruit___r_v8803.html#a90d48ac70ccff886f1f1a116a0c4b4fc',1,'Adafruit_RV8803']]],
   ['readflagregister',['readFlagRegister',['../class_adafruit___r_v8803.html#abca6b831986254e28836ae445f4067c8',1,'Adafruit_RV8803::readFlagRegister()'],['../class_adafruit___r_v8803.html#a8a7f5e3297c39f6046b5dc91aae2e901',1,'Adafruit_RV8803::readFlagRegister(uint8_t *flags)']]],
-  ['readgp',['readGP',['../class_adafruit___r_v8803.html#af43c69fdb089fc700b90f4981a76dca0',1,'Adafruit_RV8803']]],
   ['readram',['readRAM',['../class_adafruit___r_v8803.html#acdde2edf442618ce8cb5d2cd96617d35',1,'Adafruit_RV8803']]],
   ['readsqwpinmode',['readSqwPinMode',['../class_adafruit___r_v8803.html#aa89cbc4986a1c5f14925eb05c22aeb30',1,'Adafruit_RV8803']]],
   ['reset',['reset',['../class_adafruit___r_v8803.html#a14409b4a88cf98f1ac925cbbcb759286',1,'Adafruit_RV8803']]]
